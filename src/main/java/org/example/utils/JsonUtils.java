7e7fb6cb98f2395e25b4cb3ada45d169ca20b189
ghchoi <ghchoi0427@gmail.com>
2023-03-27 21:11:19 +0900

    fix: remove gson dependency

--- src/main/java/org/example/utils/JsonUtils.java
+++ src/main/java/org/example/utils/JsonUtils.java
@@ -1,19 +1,16 @@
 package org.example.utils;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static String getResponseContent(String responseBody) {
         String content = null;
         try {
-            JsonObject responseJson = JsonParser.parseString(responseBody).getAsJsonObject();
-            JsonArray choices = responseJson.getAsJsonArray("choices");
-
-            JsonObject message = choices.get(0).getAsJsonObject().getAsJsonObject("message");
-            content = message.get("content").getAsString();
+            String[] choices = getArrayFromJson(responseBody, "choices");
+            String message = getObjectFromJson(choices[0], "message");
+            content = getObjectFromJson(message, "content");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,9 +18,39 @@ public class JsonUtils {
     }
 
     public static String extractURLFromJSON(String jsonStr) {
-        JsonObject jsonObj = JsonParser.parseString(jsonStr).getAsJsonObject();
-        JsonArray dataArray = jsonObj.getAsJsonArray("data");
-        JsonObject dataObj = dataArray.get(0).getAsJsonObject();
-        return dataObj.get("url").getAsString();
+        String[] dataArray = getArrayFromJson(jsonStr, "data");
+        String dataObj = dataArray[0];
+        return getObjectFromJson(dataObj, "url");
+    }
+
+    private static String[] getArrayFromJson(String jsonString, String key) {
+        List<String> values = new ArrayList<>();
+        int start = jsonString.indexOf("\"" + key + "\":[");
+        if (start != -1) {
+            start += key.length() + 3;
+            int end = jsonString.indexOf("]", start);
+            String[] parts = jsonString.substring(start, end).split("\\{");
+            for (String part : parts) {
+                if (part.length() > 2) {
+                    values.add("{" + part);
+                }
+            }
+        }
+        return values.toArray(new String[0]);
+    }
+
+    private static String getObjectFromJson(String jsonString, String key) {
+        String value = null;
+        int start = jsonString.indexOf("\"" + key + "\":");
+        if (start != -1) {
+            start += key.length() + 3;
+            int end = jsonString.indexOf(",", start);
+            if (end == -1) {
+                end = jsonString.indexOf("}", start);
+            }
+            value = jsonString.substring(start, end);
+            value = value.replaceAll("\"", "");
+        }
+        return value;
     }
 }
